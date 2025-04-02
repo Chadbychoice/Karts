b71f971b6dab0429dd51d20201ea2c24dc2871ae
Chad Bychoice <chadbychoice@gmail.com>
2025-04-02 20:51:30 +0100

    fix: Explicitly set Socket.IO path on client

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 // --- Basic Setup ---
-const socket = io();
+const socket = io({ path: "/socket.io" });
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
